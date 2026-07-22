7970fd4918e1b016a0838adc7c2f7214773bfd51
Zain Sajid <zainsajid.056@gmail.com>
2026-07-22 21:57:10 +0500

    Update BorderAnimatedContainer.jsx

--- frontend/src/components/BorderAnimatedContainer.jsx
+++ frontend/src/components/BorderAnimatedContainer.jsx
@@ -1,11 +1,14 @@
 // How to make animated gradient border 👇
 // https://cruip-tutorials.vercel.app/animated-gradient-border/
+
 function BorderAnimatedContainer({ children, className = "" }) {
   return (
-    <div className="w-full h-full md:[background:linear-gradient(45deg,#172033,theme(colors.slate.800)_50%,#172033)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.slate.600/.48)_80%,_theme(colors.cyan.500)_86%,_theme(colors.cyan.300)_90%,_theme(colors.cyan.500)_94%,_theme(colors.slate.600/.48))_border-box] md:rounded-2xl md:border md:border-transparent md:animate-border flex overflow-hidden">
-    <div className={`w-full h-full [background:linear-gradient(45deg,#172033,theme(colors.slate.800)_50%,#172033)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.slate.600/.48)_80%,_theme(colors.cyan.500)_86%,_theme(colors.cyan.300)_90%,_theme(colors.cyan.500)_94%,_theme(colors.slate.600/.48))_border-box] rounded-2xl border border-transparent animate-border flex overflow-hidden ${className}`}>
+    <div
+      className={`w-full h-full [background:linear-gradient(45deg,#172033,theme(colors.slate.800)_50%,#172033)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.slate.600/.48)_80%,_theme(colors.cyan.500)_86%,_theme(colors.cyan.300)_90%,_theme(colors.cyan.500)_94%,_theme(colors.slate.600/.48))_border-box] rounded-2xl border border-transparent animate-border flex overflow-hidden ${className}`}
+    >
       {children}
     </div>
   );
 }
+
 export default BorderAnimatedContainer;
