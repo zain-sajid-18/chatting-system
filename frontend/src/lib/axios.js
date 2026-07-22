eaa5d5529c1517a574cfe93df010d42c7cc5d9ed
Zain Sajid <zainsajid.056@gmail.com>
2026-07-22 08:30:28 +0500

    Minor changes and make ready for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true,
 });
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,8 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
-
+const BASE_URL = import.meta.env.VITE_API_URL;
 export const useAuthStore = create((set, get) => ({
   authUser: null,
   isCheckingAuth: true,
